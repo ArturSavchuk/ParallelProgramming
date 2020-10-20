83a72982bdfcf18daefd6036e3c422b1762933ca
ArturSavchuk <31774393+ArturSavchuk@users.noreply.github.com>
2020-10-20 18:41:41 +0300

    Non-blocking algorithm

---
+++ Lab3/FindMinMax.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Lab3
+{
+
+    class MinMaxWithIndexes
+    {
+        public int Min { get; set; } = int.MaxValue;
+        public int MinIndex { get; set; }
+
+        public int Max { get; set; } = int.MinValue;
+        public int MaxIndex { get; set; }
+    }
+    class FindMinMax
+    {
+        public static int minIndex = 0;
+        public static int maxIndex = 0;
+        public static MinMaxWithIndexes FindMinMaxParallel(IList<int> sequence)
+        {
+            Enumerable.Range(0, sequence.Count())
+                .AsParallel()
+                .AsUnordered()
+                .ForAll(index =>
+                {
+                    int oldValue;
+                    int newValue;
+
+                    // min
+                    do
+                    {
+                        oldValue = minIndex;
+                        newValue = index;
+
+                        if (sequence[oldValue] <= sequence[newValue])
+                        {
+                            break;
+                        }
+
+                    } while (oldValue != Interlocked.CompareExchange(ref minIndex, newValue, oldValue));
+
+                    // max
+                    do
+                    {
+                        oldValue = maxIndex;
+                        newValue = index;
+                        if (sequence[oldValue] >= sequence[newValue])
+                        {
+                            break;
+                        }
+
+                    } while (oldValue != Interlocked.CompareExchange(ref maxIndex, newValue, oldValue));
+                });
+
+            return new MinMaxWithIndexes
+            {
+                MinIndex = minIndex,
+                Min = sequence[minIndex],
+                MaxIndex = maxIndex,
+                Max = sequence[maxIndex]
+            };
+
+        }
+    }
+}
---
+++ Lab3/HashSumCounet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Lab3
+{
+    class HashSumCounter
+    {
+        public static int sum = 0;
+
+        public HashSumCounter() { }
+        public static int Count(IEnumerable<int> sequence)
+        {
+            sequence.AsParallel().AsUnordered().ForAll(number =>
+            {
+                int initialValue, computedValue;
+                do
+                {
+                    initialValue = sum;
+
+                    computedValue = initialValue ^ number;
+                }
+                while (initialValue != Interlocked.CompareExchange(ref sum,
+                    computedValue, initialValue));
+            });
+
+            return sum;
+        }
+    }
+}
